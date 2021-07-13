c74c086ba6894f4a5b74454d9b24c83e57fa5d6b
Thiago Leite <leite.tjs@gmail.com>
2021-07-13 20:12:34 -0300

    Updates

---
+++ precache-manifest.5f9736f332d89eb1647563966c01ff91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d557ba42b9929887e59bd4cb176ea8a",
+    "url": "/wallet-project/index.html"
+  },
+  {
+    "revision": "0941e19ac1b1c5e3d4fd",
+    "url": "/wallet-project/static/css/main.802cc520.chunk.css"
+  },
+  {
+    "revision": "1ba6f407f38f61e81231",
+    "url": "/wallet-project/static/js/2.e89c9e22.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/wallet-project/static/js/2.e89c9e22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0941e19ac1b1c5e3d4fd",
+    "url": "/wallet-project/static/js/main.1f93b87e.chunk.js"
+  },
+  {
+    "revision": "58e418053cb5ee66be44",
+    "url": "/wallet-project/static/js/runtime-main.f947fa3a.js"
+  },
+  {
+    "revision": "1a3bd7320bf2bbc7147569f46ea75b61",
+    "url": "/wallet-project/static/media/5a105381eed609b127ec423c337f64e3.1a3bd732.gif"
+  }
+]);
